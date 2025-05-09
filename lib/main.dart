91996076da66d03b7e1dba70123fe11afe0a208c
chedighribi <chady.ghribi.gc@gmail.com>
2025-05-09 16:30:36 +0200

    feat: main feed card

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,36 @@ class MyHomePage extends StatelessWidget {
           // Main
           Expanded(
             child: Container(
-              color: Colors.black45,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 125,
+                        child: Image.network("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("pseudo"),
+                                  Text("5 min"),
+                                ],
+                              ),
+                              Text("ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ici commentaire ")
+                            ]
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              
             ),
           ),
           // Footer
