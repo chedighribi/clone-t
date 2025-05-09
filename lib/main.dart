1c555c3b04e3a6964c179a2c2a32b418bce92f8f
chedighribi <chady.ghribi.gc@gmail.com>
2025-05-09 11:49:12 +0200

    feat: header and footer

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,14 +20,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class HeaderButton extends StatelessWidget {
+
+  String libelle;
+  TextAlign align;
+  HeaderButton(this.libelle, {this.align = TextAlign.center });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Text(libelle, textAlign: this.align, style: TextStyle(color: Colors.white),));
+  }
+}
+
+class FooterButton extends StatelessWidget {
+
+  String libelle;
+  FooterButton(this.libelle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Text(libelle, textAlign: TextAlign.center));
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("test"),
+        title: Text("S-Clone"),
       ),
-      body: Text("todo")
+      body: Column(
+        children: [
+          // Header
+          Container(
+            color: Color(0XFF5880F0),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  HeaderButton('Nouveau', align: TextAlign.right),
+                  HeaderButton('Accueil'),
+                  HeaderButton('Footer', align: TextAlign.left),
+                ],
+              ),
+            ),
+          ),
+          // Main
+          Expanded(
+            child: Container(
+              color: Colors.black45,
+            ),
+          ),
+          // Footer
+          Container(
+            color: Color(0XFF5880F0),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  FooterButton('Fil'),
+                  FooterButton('Notification'),
+                  FooterButton('Message'),
+                  FooterButton('Moi'),
+                ],
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
